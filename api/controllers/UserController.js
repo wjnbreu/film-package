60c104dbacb3b357e6a59ae4961ed45ad18e7bb5
James Pants <jamespants@gmail.com>
2014-01-21 15:20:00 +0100

    added user model controller

---
+++ api/controllers/UserController.js
@@ -0,0 +1,111 @@
+/**
+ * UserController
+ *
+ * @module      :: Controller
+ * @description	:: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+
+	'new': function(req, res){
+		res.view();
+	},
+
+	create: function (req, res, next){
+		//create a user with params sent from
+		//the signup form new.ejs
+		User.create( req.params.all(), function userCreated (err, user){
+
+			//if there's an error
+			if (err){
+				console.log(err);
+				req.session.flash = {
+					err: err
+				};
+
+
+				//if error,redirect to signup page
+				return res.redirect('/user/new');
+			}
+
+			//Log user in
+			req.session.authenticated = true;
+			req.session.User = user;
+
+			//after success, redirect to show action
+			res.redirect('/user/show/'+user.id);
+		});
+	},
+
+	show: function(req, res, next){
+		User.findOne(req.param('id'), function foundUser (err, user){
+			if (err) return next(err);
+			if (!user) return next('User doesn\'t exist.');
+			res.view({
+				user: user
+			});
+		});
+	},
+
+	index: function(req, res, next){
+
+		//get an array of all users in the User db
+		User.find(function foundUsers(err, users){
+			if (err) return next(err);
+
+			//pass array down to index.ejs page
+			res.view({
+				users: users
+			});
+		});
+	},
+
+	//edit.ejs
+	edit: function(req, res, next){
+		//find the user from the id passed in via paramenters
+		User.findOne(req.param('id'), function foundUser(err, user){
+			if (err) return next(err);
+			if (!user) return next('User doesn\'t exist.');
+
+			res.view({
+				user: user
+			});
+		});
+	},
+
+	//process the info from the edit view
+	update: function (req, res, next){
+		User.update(req.param('id'), req.params.all(), function userUpdated(err){
+			if (err) {
+				return res.redirect('/user/edit/' + req.param('id'));
+			}
+
+			res.redirect('/user/show/' + req.param('id'));
+		});
+	},
+
+	destroy: function(req, res, next){
+		User.findOne(req.param('id'), function foundUser(err, user){
+			if (err) return next(err);
+
+			if (!user) return next('User doesn\'t exist.');
+
+			User.destroy(req.param('id'), function userDestroyed(err){
+				if (err) return next(err);
+			});
+
+			res.redirect('/user');
+		});
+	}
+  
+};
---
+++ api/models/User.js
@@ -0,0 +1,77 @@
+/**
+ * User
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+	schema: true,
+
+  attributes: {
+	
+	name: {
+		type: 'string',
+		required: true
+	},
+
+	title: {
+		type: 'string'
+	},
+
+	email: {
+		type: 'string',
+		email: true,
+		//required: true,
+		unique: true
+	},
+
+	admin: {
+		type: 'boolean',
+		defaultsTo: false
+	},
+
+	encryptedPassword: {
+		type: 'string'
+	},
+
+	toJSON: function(){
+		var obj = this.toObject();
+		delete obj.password;
+		delete obj.confirmation;
+		delete obj.encryptedPassword;
+		delete obj._csrf;
+		return obj;
+	}
+	
+  },
+
+  beforeValidation: function(values, next){
+	if (typeof values.admin != 'undefined'){
+		if (values.admin === 'unchecked'){
+			values.admin = false;
+		}
+		else if (values.admin[1] === 'on'){
+			values.admin = true;
+		}
+	}
+	next();
+  },
+
+  beforeCreate: function (values, next){
+
+	//this checks to make sure password and conf match before creating record
+	if (!values.password || values.password != values.confirmation){
+		return next({err: ["Passwords not matching."]});
+	}
+
+	require('bcrypt').hash(values.password, 10, function passwordEncrypted(err, encryptedPassword){
+		if (err) return next(err);
+		values.encryptedPassword = encryptedPassword;
+		next();
+	});
+  }
+
+};
