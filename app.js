274670b9a0e398dee51daa39ad9637019fb6a95c
James Pants <jamespants@gmail.com>
2014-07-09 20:50:19 +0200

    change hooks disable ws

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 // Start sails and pass it command line arguments
-require('sails').lift(require('optimist').argv);
+
+var config = sails.util.merge(require('optimist').argv,{
+	hooks: {
+		sockets: false,
+		pubsub: false
+	}
+});
+
+require('sails').lift(config);
